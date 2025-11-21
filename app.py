b379882e8b5b8b81ed8d50c8dfae21dc43157aa4
subhanavya <subhasreenavya.v@gmail.com>
2025-11-22 02:44:49 +0530

    Update sidebar

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ region_df = pd.read_csv('noc_regions.csv')
 df = preprocessor.preprocess(df,region_df)
 
 st.sidebar.title("Olympics Analysis")
-st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
+st.sidebar.image('https://img.olympics.com/images/image/private/t_social_share_thumb/f_auto/primary/mmdfgqbwukdh3hmvs5rn')
 user_menu = st.sidebar.radio(
     'Select an Option',
     ('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis')
